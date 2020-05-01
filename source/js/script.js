8ffc10a4775470680377dbef6407ab8e0fb6490d
Podivilov Evgeniy <evgeniy.podivilov@gmail.com>
2020-05-01 14:17:23 +0300

    feat(js): add loaded class to body

--- source/js/script.js
+++ source/js/script.js
@@ -21,3 +21,9 @@ social();
 
 const fullPageScroll = new FullPageScroll();
 fullPageScroll.init();
+
+window.addEventListener(`load`, () => {
+  setTimeout(() => {
+    document.body.classList.add(`loaded`);
+  }, 200);
+});
